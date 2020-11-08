f12057317ff1cd1c5eb321f82497b6b3a24d806d
Erick Tamayo <ericktamayo@Ericks-MBP.hitronhub.home>
2020-11-08 11:48:00 -0700

    removed jest comments

--- jest.config.js
+++ jest.config.js
@@ -5,23 +5,10 @@ module.exports = {
   preset: "react-native",
   testEnvironment: "node",
   transform: {
-    // ...tsjPreset.transform,
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.jsx?$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
   },
-  // cacheDirectory: "<rootDir>/.jest/cache",
-  // setupFiles: ["<rootDir>/jest.setup.js"],
-  // setupFilesAfterEnv: ["@testing-library/react-native/cleanup-after-each"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/plugin/"],
-  // transformIgnorePatterns: [
-  //   "node_modules/(?!(react-native" +
-  //     "|react-navigation-tabs" +
-  //     "|react-native-splash-screen" +
-  //     "|react-native-screens" +
-  //     "|react-native-reanimated" +
-  //     "|@testing-library/react-hooks" +
-  //     ")/)",
-  // ],
   globals: {
     "ts-jest": {
       babelConfig: true,
